3df5fdf8da1f1021c40cc5d7dd43e6cef53e3b7e
Rohit Jones <RohitJones@users.noreply.github.com>
2017-10-07 11:32:31 -0400

    instance config and removed un-necessary stuff

--- application/views/hymns/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-hymns = Blueprint('hymns', __name__, url_prefix='/hymns')
-
-from . import views
--- application/views/hymns/helper.py
+++
@@ -1,9 +0,0 @@
-from os.path import join
-
-
-def check(open_file, number, path):
-    try:
-        open_file(join(path, 'static', 'hymns', '{}.txt'.format(number)), 'r')
-    except FileNotFoundError:
-        return False
-    return True
