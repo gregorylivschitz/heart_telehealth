8ce314365bd32178094c6a15eaadca3950c57caa
Rohit Jones <rohitjones@users.noreply.github.com>
2017-10-07 17:17:09 -0400

    partial work on doctor is done.

--- application/views/doctor/views.py
+++ application/views/doctor/views.py
@@ -10,13 +10,58 @@ from . import doctor
 def doctor_dashboard(doctor_id):
     # retived doctor object
 
-    mock_pat = {
-        111: {
-            'name': "patient 1",
+    mock_pat = [
+        {
+            'id': 111,
+            'name': "Jimmy",
             'age': 56,
-            'pings': 100
+            'weight': 85,
+            'email': "jimmy@jimmy.com",
+            'tel': 1234567890,
+            'pings': 4,
+            'high': 633,
+            'low': 451,
+            'last_ping': '2016-10-28'
+        },
+        {
+            'id': 112,
+            'name': "Sammy",
+            'age': 47,
+            'weight': 110,
+            'email': "sammy@sammy.com",
+            'tel': 6546541561,
+            'pings': 7,
+            'high': 721,
+            'low': 523,
+            'last_ping': '2017-04-18'
+        },
+        {
+            'id': 124,
+            'name': "Greg",
+            'age': 29,
+            'weight': 75,
+            'email': "greg@greg.com",
+            'tel': 4513576512,
+            'pings': 2,
+            'high': 672,
+            'low': 510,
+            'last_ping': '2016-11-15'
+        },
+        {
+            'id': 278,
+            'name': "Benny",
+            'age': 62,
+            'weight': 79,
+            'email': "benny@benny.com",
+            'tel': 8794506123,
+            'pings': 8,
+            'high': 607,
+            'low': 492,
+            'last_ping': '2017-02-10'
         }
-    }
+    ]
 
-    return render_template('doctor/dashboard.html', doctor=None, doctor_id=doctor_id, patients=None)
+    mock_pat = sorted(mock_pat, key=lambda x: x['last_ping'], reverse=True)
+
+    return render_template('doctor/dashboard.html', doctor=None, doctor_id=doctor_id, patients=mock_pat)
     # doctor_obj[doctor_id].patients
