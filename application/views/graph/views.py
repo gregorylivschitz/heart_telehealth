3877d98eb95ecfdfe6d775a5f523bd7601f84a0e
mrpoor <greg15001@gmail.com>
2017-10-07 16:05:42 -0400

    added graph

--- application/views/graph/views.py
+++ application/views/graph/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
+import random
+
+from flask import render_template, jsonify
 from flask_login import login_required
 import pandas as pd
 from . import graph
-
 
 import numpy as np
 from scipy.integrate import simps
@@ -16,26 +17,41 @@ def graph():
     # tone_data = pd.read_csv('tone_data')
 
     multiple_tone = []
-    for i in range(0,20):
-        time_date = datetime.datetime.now()
-        rand.in
-        tone_data = pd.DataFrame({'tone': [105, 115, 130, 120, 122, 105], 'time':[time_date,
-                                                                    tone_data + datetime.timedelta(seconds=1),
-                                                                    tone_data + datetime.timedelta(seconds=2),
-                                                                    tone_data + datetime.timedelta(seconds=3),
-                                                                    tone_data + datetime.timedelta(seconds=4),
-                                                                    tone_data + datetime.timedelta(seconds=5)]})
+    for index, i in enumerate(range(0,24)):
+        time_date = datetime.datetime.now() + datetime.timedelta(seconds=index)
+        print(time_date)
+        tone_data = pd.DataFrame({'tone': [105 , 115, 130, 120, 122, 105], 'time':[time_date,
+                                                                                   time_date + datetime.timedelta(seconds=1),
+                                                                                   time_date + datetime.timedelta(seconds=2),
+                                                                                   time_date + datetime.timedelta(seconds=3),
+                                                                                   time_date + datetime.timedelta(seconds=4),
+                                                                                   time_date + datetime.timedelta(seconds=5)]})
+        print(time_date + datetime.timedelta(seconds=1))
+        tone_data['rand_num'] = np.random.randint(0,5, size=len(tone_data))
+        tone_data['tone'] = tone_data['tone'] + tone_data['rand_num']
         multiple_tone.append(tone_data)
+
     multiple_tone_df = pd.concat(multiple_tone, ignore_index=True)
     multiple_tone_df.to_csv("C:\\Users\\greg1\\dev\\heart_telehealth\\test_data.csv")
 
-    # The y values.  A numpy array is used here,
-    # but a python list could also be used.
-    y = np.array([135, 125, 120, 115, 117])
-
-    # Compute the area using the composite trapezoidal rule.
-    area = trapz(y, dx=1)
-    # Compute the area using the composite Simpson's rule.
-    area = simps(y, dx=1)
-    print("area =", area)
-    return render_template('graph/output.html', area=area)
+    size=len(multiple_tone_df)/6
+    size = np.math.floor(size)
+    add_integral = []
+    labels = []
+    values = []
+    for i in range(0,size,6):
+        if i == 0:
+            integral_calc = multiple_tone[i:i+5]
+        else:
+            integral_calc = multiple_tone[i-5:i]
+        # numpy_values = integral_calc.values
+        time_things = integral_calc[0].ix[0, ('time')]
+        y = integral_calc[0].loc[:, ('tone')]
+        area = simps(y, dx=1)
+        add_integral.append((area, time_things))
+        for value, label in add_integral:
+            labels.append(label)
+            values.append(value)
+    print(labels)
+    print(values)
+    return render_template('graph/output.html', values=values, labels=labels)
